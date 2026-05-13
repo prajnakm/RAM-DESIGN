7536266ea116a422e445350404e2bda008916bc0
prajnakm <roxy71791@gmail.com>
2026-05-13 21:50:58 +0530

    Update synch_ram.v

--- synch_ram.v
+++ synch_ram.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.05.2026 13:36:36
-// Design Name: 
-// Module Name: synch_ram
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module synch_ram(
     input clk,
     input we,
@@ -39,3 +18,5 @@ begin
 end
 
 endmodule
+
+//
